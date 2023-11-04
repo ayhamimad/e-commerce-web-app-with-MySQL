bc80698ccfd0d5c72bd417457a05930e576615d1
ayhamimad <ayhamihuw@gmail.com>
2023-11-04 23:45:47 +0200

    handeled the productsController

--- app/routers/product.route.ts
+++ app/routers/product.route.ts
@@ -2,9 +2,9 @@ const express = require('express');
 import { Request, Response } from 'express';
 const router = express.Router();
 import * as searchController from "../controllers/search.controller";
-
+import * as productsController from "../controllers/products.controller"
 router.get('/search',searchController.search);
-//router.get('/products',productController.search);
+router.get('/products',productsController.list);
 
-
+//assets/images/brands/Zara_Logo 1.png
 export default router;
