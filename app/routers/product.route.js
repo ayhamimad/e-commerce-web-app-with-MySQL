d0fddd77d7e7197af999231ebe452fa7b0f80035
OmarIsmailIT <omar.ism65@gmail.com>
2023-11-04 19:35:18 +0200

    search js

--- app/routers/product.route.js
+++ app/routers/product.route.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var router = express.Router();
-var productController = require("../controllers/product.controller");
-router.get('/search', productController.search);
+var searchController = require("../controllers/search.controller");
+router.get('/search', searchController.search);
+//router.get('/products',productController.search);
 exports.default = router;
