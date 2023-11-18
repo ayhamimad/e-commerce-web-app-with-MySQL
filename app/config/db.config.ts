3f74281a2afe8faa62ffc61dfbf8041ea858a24b
ayhamimad <ayhamihuw@gmail.com>
2023-11-18 21:12:57 +0200

    finishhhhhh

--- app/config/db.config.ts
+++ app/config/db.config.ts
@@ -1,23 +1,8 @@
-// export const dbConfig = {
-//     HOST: "sql.freedb.tech",
-//     USER: "freedb_backend-final-3", // Replace with your MySQL username
-//     PASSWORD: "*NgvyAs9f#Dnyd&", // Replace with your MySQL password
-//     DB: "freedb_coral_ecommerce", // Replace with your MySQL database name
-//     DIALECT: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-//   };
-
-
-  export const dbConfig = {
-    HOST: "localhost",
-    USER: "root", // Replace with your MySQL username
-    PASSWORD: "ayham123", // Replace with your MySQL password
-    DB: "coral_ecommerce", // Replace with your MySQL database name
+export const dbConfig = {
+    HOST: "sql.freedb.tech",
+    USER: "freedb_backend-final-3", // Replace with your MySQL username
+    PASSWORD: "*NgvyAs9f#Dnyd&", // Replace with your MySQL password
+    DB: "freedb_coral_ecommerce", // Replace with your MySQL database name
     DIALECT: "mysql",
     pool: {
         max: 5,
@@ -26,4 +11,19 @@
         idle: 10000
     }
   };
+
+
+//   export const dbConfig = {
+//     HOST: "localhost",
+//     USER: "root", // Replace with your MySQL username
+//     PASSWORD: "ayham123", // Replace with your MySQL password
+//     DB: "coral_ecommerce", // Replace with your MySQL database name
+//     DIALECT: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+//   };
 
