3d01254c2676b7f951e1e784397f15d8f4b709d3
ayhamimad <ayhamihuw@gmail.com>
2023-11-22 08:52:30 +0200

    change tokken to 2m

--- app/config/jwt.config.ts
+++ app/config/jwt.config.ts
@@ -1,5 +1,5 @@
 export const jwtConfig = {
     secret: 'top-secret', // Secret key for signing JWT tokens
-    expiresIn: '1h', // Token expiration time
+    expiresIn: '1M', // Token expiration time
   };
   
